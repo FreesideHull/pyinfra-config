c1d927e94e7d82eaf8e1fddb8e8e736c04d44948
CrimsonTome <crimsontome427@protonmail.com>
2022-07-24 14:44:43 +0100

    explain commands.py

---
+++ commands.py
@@ -0,0 +1,6 @@
+# add your commands here, they must follow pyinfra's syntax!
+# you cannot just add `sudo apt update` or other in here. 
+# if you want to do that then run `pyinfra hosts.py exec -- your-command-here`
+# if you need sudo privalages then `pyinfra hosts.py --sudo exec -- your-command-here`
+# for pyinfras syntax, see their docs page: https://docs.pyinfra.com
+# for an example for apt see: https://github.com/Fizzadar/pyinfra/blob/2.x/examples/apt.py
