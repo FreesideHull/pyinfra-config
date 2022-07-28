222f758fefdbdba0784b29e87ff2c0fbf31a90a0
CrimsonTome <crimsontome427@protonmail.com>
2022-07-28 20:23:12 +0100

    add verify home dir snippet

---
+++ commands/verify-home-dir.py
@@ -0,0 +1,18 @@
+from pyinfra.operations import server, files
+
+username = input("enter the username of the account you wish to verify: ")
+
+server.user(
+    name="Validate user", # or create them if they somehow don't exist
+    user=username,
+    home="/home/"+username,
+    _sudo=True,
+)
+
+files.directory(
+    name="Ensure /home/username exists", # create the folder, if its a new user it is unlikely to exist
+    path="/home/"+username,
+    user=username,
+    group=username,
+    _sudo=True,
+)
