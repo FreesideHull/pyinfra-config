249b82dae1332de2beeca136ace1b23f80b0c7ee
CrimsonTome <crimsontome427@protonmail.com>
2022-10-29 00:30:31 +0100

    chore: tweak reboot reconnect delay

--- commands/update.py
+++ commands/update.py
@@ -1,5 +1,5 @@
 from pyinfra.operations import dnf, server
 
 dnf.update() # assumes the target(s) use RHEL based distros. Here at Freeside we use Fedora. replace with whatever package manager you use on your targets. pyinfra can do its own scanning to find which package manager is in use to automate that for you - pretty cool huh 
-server.reboot(delay=10, interval=1, reboot_timeout=300) # will reboot the target(s) and try to reconnect to them
+server.reboot(delay=15, interval=1, reboot_timeout=300) # will reboot the target(s) and try to reconnect to them
 # any post reboot commands here
