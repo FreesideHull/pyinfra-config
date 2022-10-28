921ceadbbb9af9b232dd97e693d4216db684535e
CrimsonTome <crimsontome427@protonmail.com>
2022-10-29 00:57:58 +0100

    refactor: put df -h in its own method

--- commands/update.py
+++ commands/update.py
@@ -1,14 +1,12 @@
 from pyinfra.operations import dnf, server
 
-
-server.shell(
+def dfh():
+    server.shell(
     name="check disk space,
     commands=["df -h],
 )
+
 dnf.update() # assumes the target(s) use RHEL based distros. Here at Freeside we use Fedora. replace with whatever package manager you use on your targets. pyinfra can do its own scanning to find which package manager is in use to automate that for you - pretty cool huh 
 server.reboot(delay=15, interval=1, reboot_timeout=300) # will reboot the target(s) and try to reconnect to them
 # any post reboot commands here
-server.shell(
-    name="check disk space,
-    commands=["df -h],
-)
+dfh()
