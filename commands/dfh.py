ea8b360ba19860693da5d932e90351e2ca0a8ad3
CrimsonTome <crimsontome427@protonmail.com>
2022-11-14 21:31:25 +0000

    feat: facts work now, can get output from commands!

---
+++ commands/dfh.py
@@ -0,0 +1,15 @@
+from pyinfra.api import FactBase
+from pyinfra import host
+class raw(FactBase):
+    '''
+    run raw command
+    '''
+
+    def command(self, command):
+        return command
+
+    def process(self, output):
+        return '\n'.join(output)
+
+dfh_out = host.get_fact(raw, command='df -h')
+print(dfh_out)
