621014798b79b112121f531f6e6fc6bb6e803e26
CrimsonTome <crimsontome427@protonmail.com>
2022-11-18 00:04:20 +0000

    refactor: docker purge as fact so we get proper output

--- commands/docker-purge.py
+++ commands/docker-purge.py
@@ -1,6 +1,15 @@
-from pyinfra.operations import server
+from pyinfra.api import FactBase
+from pyinfra import host
+class raw(FactBase):
+    '''
+    run raw command
+    '''
 
-server.shell(
-    name="purge all unused docker assets",
-    commands=["docker system prune -fa"],
-)
+    def command(self, command):
+        return command
+
+    def process(self, output):
+        return '\n'.join(output)
+
+vac_out = host.get_fact(raw, command="docker system prune -fa")
+print(vac_out)
