a845d2d5a17804010ed5a75adacbdf519f804868
CrimsonTome <crimsontome427@protonmail.com>
2022-11-18 00:10:06 +0000

    refactor: update as fact - untested

--- commands/update.py
+++ commands/update.py
@@ -1,15 +1,18 @@
-from pyinfra.operations import dnf, server
+from pyinfra.operations import server
+from pyinfra import host
+class raw(FactBase):
+    '''
+    run raw command
+    '''
+
+    def command(self, command):
+        return command
+
+    def process(self, output):
+        return '\n'.join(output)
+
+update_out = host.get_fact(raw, command="dnf upgrade --refresh -y")
+print(update_out)
 
 
-# there is a way to define your own operations but i'm yet to look at that yet
-def dfh():
-    server.shell(
-    name="check disk space",
-    commands=["df -h"],
-)
-
-# above works but gives no output, look into Facts
-
-dnf.update() # assumes the target(s) use RHEL based distros. Here at Freeside we use Fedora. replace with whatever package manager you use on your targets. pyinfra can do its own scanning to find which package manager is in use to automate that for you - pretty cool huh 
 server.reboot(delay=15, interval=1, reboot_timeout=300) # will reboot the target(s) and try to reconnect to them
-# any post reboot commands here
